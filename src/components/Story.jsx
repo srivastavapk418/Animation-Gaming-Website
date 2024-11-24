c758cc91941a8db805dcaa63f1d63fb99eaaa863
srivastavapk418 <144578868+srivastavapk418@users.noreply.github.com>
2024-11-24 16:44:35 +0530

    Update Story.jsx
    just updated the src from /img to img

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -65,7 +65,7 @@ const Story = () => {
                             onMouseUp={handleMouseLeave}
                             onMouseEnter={handleMouseLeave}
                             onMouseMove={handleMouseMove}
-                            src="/img/entrance.webp" 
+                            src="img/entrance.webp" 
                             alt="entrance" 
                             className='object-contain'/>
                         </div>
