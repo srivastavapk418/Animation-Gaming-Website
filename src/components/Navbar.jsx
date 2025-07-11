a31fd42b5a94a28dc08e1b08a6127dfc8a4894c9
srivastavapk418 <144578868+srivastavapk418@users.noreply.github.com>
2025-07-11 23:04:56 +0530

    Update Navbar.jsx
    To play audio just by starting the website

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import gsap from 'gsap';
 const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact'];
 
 const Navbar = () => {
-    const [isAudioPlaying, setIsAudioPlaying] = useState(false)
+    const [isAudioPlaying, setIsAudioPlaying] = useState(true)
     const [isIndicatorActive, setIsIndicatorActive] = useState(false)
     const [lastScrollY, setLastScrollY] = useState(0)
     const [isNavVisible, setIsNavVisible] = useState(true)
